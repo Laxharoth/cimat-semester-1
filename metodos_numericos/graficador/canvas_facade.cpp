388e1a188fe3cc6cb58cf1fe80e3c8db6df59dec
Laxharoth <zeydenlaxharoth@gmail.com>
2022-08-09 17:21:51 -0500

    Update draw_line
    -prevent a line to be drawn if either point has a nan value
    -added scale to each x and y value

--- metodos_numericos/graficador/canvas_facade.cpp
+++ metodos_numericos/graficador/canvas_facade.cpp
@@ -4,12 +4,14 @@ namespace canvas_facade{
         this->origin.x = new_origin.x;
         this->origin.y = new_origin.y;
     }
-    cairo_set_line_width(cr, 1.0);
     void CanvasFacade::draw_line(cairo_t *cr, const point &point_1, const point &point_2) const{
+        if(point_1.is_nan || point_2.is_nan) return;
+        cairo_set_line_width(cr, 1.0);
         cairo_set_source_rgb(cr,0.0,0.0,0.0);
-        cairo_move_to(cr, point_1.x - this->origin.x, this->origin.y - point_1.y);
-        cairo_line_to(cr, point_2.x - this->origin.x, this->origin.y - point_2.y);
-        //g_printerr("(%f,%f)->(%f,%f)\n",this->origin.x + point_1.x * x_scale, this->origin.y - point_1.y * y_scale, this->origin.x + point_2.x * x_scale, this->origin.y - point_2.y * y_scale);
+        cairo_move_to(cr, point_1.x * scale_x - this->origin.x, this->origin.y - point_1.y * scale_y);
+        cairo_line_to(cr, point_2.x * scale_x - this->origin.x, this->origin.y - point_2.y * scale_y);
+        cairo_stroke(cr);
+    }
         cairo_stroke(cr);
     }
     point choose_origin_helper( const double &min_x, const double &min_y,
