3dd488befbf7d807859ebf9f3713de77829de8f0
Laxharoth <zeydenlaxharoth@gmail.com>
2022-08-16 20:17:43 -0500

    Fixed multiple declaration and include issues

--- metodos_numericos/busqueda_de_raiz/function_wrapper/parser_wrapper.cpp
+++ metodos_numericos/busqueda_de_raiz/function_wrapper/parser_wrapper.cpp
@@ -1,8 +1,5 @@
-#ifndef PARSER_WRAPPER_CPP
-#define PARSER_WRAPPER_CPP
 #include "parser_wrapper.hpp"
 #include "function_wrapper.cpp"
+
 FunctionParserAdapter_to_FWrapper::FunctionParserAdapter_to_FWrapper(FunctionParser *parser):parser(parser) {}
 double FunctionParserAdapter_to_FWrapper::eval(const double &x){ return parser->Eval(&x); }
-
-#endif /* PARSER_WRAPPER_CPP */
--- metodos_numericos/graficador/canvas_facade.hpp
+++ metodos_numericos/graficador/canvas_facade.hpp
@@ -22,14 +22,6 @@ point choose_origin_helper( const double &min_x, const double &min_y,
                             const double &scale_x, const double &scale_y,
                             const double &canvas_heigh);
 double choose_scale_helper(const double &min, const double &max, const double &space);
-point choose_origin_helper( const double &min_x, const double &min_y,
-                                const double &scale_x, const double &scale_y,
-                                const double &canvas_heigh){
-        return point{ min_x * scale_x, min_y * scale_y + canvas_heigh , false };
-    }
-double choose_scale_helper(const double &min, const double &max, const double &space){
-    return space / (max - min);
-}
 }
 
 #endif /* CANVAS_FACADE_HPP */
