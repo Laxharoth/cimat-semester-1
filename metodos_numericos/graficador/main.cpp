cc551a31dfb23fbe111975b309e0c1e6381a2ce1
Laxharoth <zeydenlaxharoth@gmail.com>
2022-08-18 15:09:07 -0500

    Agregados botones para mostrar derivada
    Agregado function_wrapper y parser_wrapper
    FunctionParser parser se guardo en un parser_wrapper
    Agregada  primera y segunda derivada de la funcion FunctionParser
    Agregados botones para cambiar entre la funcion original y las primeras 2 derivadas

--- metodos_numericos/graficador/main.cpp
+++ metodos_numericos/graficador/main.cpp
@@ -1,7 +1,9 @@
 #include "point.hpp"
 #include "simple_function_analizer.hpp"
 #include "canvas_facade.hpp"
-#include "fparser.hh"
+#include "function_wrapper/function_wrapper.hpp"
+#include "function_wrapper/parser_wrapper.hpp"
+#include "fparser/fparser.hh"
 
 #include <array>
 #include <iostream>
@@ -28,18 +30,33 @@ void on_btn_graph_click(GtkWidget *btn, gpointer data);
  * @brief convierte un double a string con n puntos decimales (no redondea)
  */
 std::string trim_double_to_str(const double &num, const int &precision);
+/**
+ * @brief *cambia la funcion que se muestra
+ */
+void on_radio_original_toggle(GtkWidget *widget, gpointer data);
 void on_destroy();
 
 CanvasFacade drawer;
 FunctionParser parser;
+FunctionWrapper *current_function;
+FunctionParserAdapter_to_FWrapper original(&parser);
+Derivative derivative(&original);
+Derivative derivative_2(&derivative);
+
 std::array<point, PARTITIONS_NUM+1> points;
 GtkWidget  *graph;
+GtkWidget  *btn_graph;
 GtkWidget  *min_x_limit;
 GtkWidget  *max_x_limit;
 GtkWidget  *function_rep;
+GtkWidget  *radio_original;
+GtkWidget  *radio_derivada;
+GtkWidget  *radio_derivada_2;
+
 auto func = [](double x){ return 1/x; };
 
 gint main(int argc, char *argv[]){
+    current_function = &original;
     gtk_init(&argc, &argv);
     GtkBuilder *builder = gtk_builder_new_from_file("graph.glade");
     GtkWidget  *window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
@@ -47,16 +64,22 @@ gint main(int argc, char *argv[]){
     min_x_limit = GTK_WIDGET(gtk_builder_get_object(builder, "min_x_limit"));
     max_x_limit = GTK_WIDGET(gtk_builder_get_object(builder, "max_x_limit"));
     function_rep = GTK_WIDGET(gtk_builder_get_object(builder, "function_rep"));
-    GtkWidget  *btn_graph = GTK_WIDGET(gtk_builder_get_object(builder, "btn_graph"));
+    radio_original = GTK_WIDGET(gtk_builder_get_object(builder, "radio_original"));
+    radio_derivada = GTK_WIDGET(gtk_builder_get_object(builder, "radio_derivada"));
+    radio_derivada_2 = GTK_WIDGET(gtk_builder_get_object(builder, "radio_derivada_2"));
+    btn_graph = GTK_WIDGET(gtk_builder_get_object(builder, "btn_graph"));
     g_object_unref(builder);
     g_signal_connect(window, "destroy", G_CALLBACK(on_destroy), NULL);
     g_signal_connect(graph, "draw", G_CALLBACK(on_graph_draw), NULL);
     g_signal_connect(btn_graph, "clicked", G_CALLBACK(on_btn_graph_click), NULL);
+    g_signal_connect(radio_original, "toggled", G_CALLBACK(on_radio_original_toggle), (gpointer)"1");
+    g_signal_connect(radio_derivada, "toggled", G_CALLBACK(on_radio_original_toggle), (gpointer)"2");
+    g_signal_connect(radio_derivada_2, "toggled", G_CALLBACK(on_radio_original_toggle), (gpointer)"3");
     gtk_window_set_keep_above( GTK_WINDOW(window), FALSE );
     gtk_widget_show(window);
 
     //dibuja la grafica por primera vez
-    init_points(graph, func, -1.0, 1.0);
+    on_btn_graph_click( btn_graph, NULL );
 
     gtk_main();
 
@@ -126,7 +149,7 @@ void init_points(GtkWidget *graph, double (*single_variable_func)(double), doubl
     if(min_x > max_x) std::swap(min_x, max_x);
     drawer.min_value.x = min_x; 
     drawer.max_value.x = max_x;
-    analize_single_var_function([](double x){return parser.Eval(&x);}, min_x, max_x, drawer.min_value.y, drawer.max_value.y, points);
+    analize_single_var_function([](double x){return current_function->eval(x);}, min_x, max_x, drawer.min_value.y, drawer.max_value.y, points);
     gtk_widget_queue_draw(graph);
 }
 
@@ -151,6 +174,12 @@ void on_btn_graph_click(GtkWidget *btn, gpointer data){
         if(min_x == max_x)return;
         init_points(graph, func, min_x, max_x);
     }catch(std::exception& e){}
+}
+void on_radio_original_toggle(GtkWidget *widget, gpointer data){
+    if(data == "1"){ current_function = &original; }
+    if(data == "2"){ current_function = &derivative; }
+    if(data == "3"){ current_function = &derivative_2; }
+    on_btn_graph_click( btn_graph, NULL );
 }
 std::string trim_double_to_str(const double &num, const int &precision){
     std::string n_to_s = std::to_string(num);
