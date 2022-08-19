fc0dbc8ef1d7afb0bfdfd982c7be9ca7d1c1090b
Laxharoth <zeydenlaxharoth@gmail.com>
2022-08-19 09:07:57 -0500

    Modified lambda in factorization functions
    prevent create variable by using references to the values that are going to be modified

--- metodos_numericos/funciones_matriz_2/factorizacion.cpp
+++ metodos_numericos/funciones_matriz_2/factorizacion.cpp
@@ -1,46 +1,44 @@
 #include "factorizacion.hpp"
 
 void metodo_de_crout(double **matriz, double **matriz_inferior, double **matriz_superior, const int &size){
-    auto obtener_suma_inferior = [&](const int &i, const int &j){
-        double sum{}; 
+    auto calcular_factor_inferior = [&](const int &i, const int &j){
+        matriz_inferior[i][j] = matriz[i][j];
         for(int k = 0; k <= j-1; ++k)
-            sum += matriz_inferior[i][k] * matriz_superior[k][j]; 
-        return sum;
+            matriz_inferior[i][j] -= matriz_inferior[i][k] * matriz_superior[k][j]; 
     };
-    auto obtener_suma_superior = [&](const int &i, const int &j){
-        double sum{}; 
+    auto calcular_factor_superior = [&](const int &i, const int &j){
+        matriz_superior[i][j] = matriz[i][j];
         for(int k = 0; k <= i-1; ++k)
-            sum += matriz_inferior[i][k] * matriz_superior[k][j];         
-        return sum;
+            matriz_superior[i][j] -= matriz_inferior[i][k] * matriz_superior[k][j];         
+        matriz_superior[i][j] /= matriz_inferior[i][i];
     };
     for(int i=0; i<size; ++i){
         for(int j=0; j<i; ++j){
-            matriz_inferior[i][j] = matriz[i][j] - obtener_suma_inferior(i,j);
-            matriz_superior[j][i] = (matriz[j][i] - obtener_suma_superior(j,i)) / matriz_inferior[j][j];            
+            calcular_factor_inferior(i,j);
+            calcular_factor_superior(j,i);
         }
-        matriz_inferior[i][i] = matriz[i][i] - obtener_suma_inferior(i,i);
+        calcular_factor_inferior(i,i);
         if(matriz_inferior[i][i] == 0) throw cant_factor_exception();
     }
 }
 void metodo_de_doolittle(double **matriz, double **matriz_inferior, double **matriz_superior, const int &size){
-    auto obtener_suma_superior = [&](const int &i, const int &j){
-        double sum{}; 
+    auto calcular_factor_superior = [&](const int &i, const int &j){
+        matriz_superior[i][j] = matriz[i][j];
         for(int k = 0; k <=i-1; ++k)
-            sum += matriz_inferior[i][k] * matriz_superior[k][j]; 
-        return sum;
+            matriz_superior[i][j] -= matriz_inferior[i][k] * matriz_superior[k][j]; 
     };
-    auto obtener_suma_inferior = [&](const int &i, const int &j){
-        double sum{}; 
+    auto calcular_factor_inferior = [&](const int &i, const int &j){
+        matriz_inferior[i][j] = matriz[i][j];
         for(int k = 0; k <=j-1; ++k)
-            sum += matriz_inferior[i][k] * matriz_superior[k][j]; 
-        return sum;
+            matriz_inferior[i][j] -= matriz_inferior[i][k] * matriz_superior[k][j]; 
+        matriz_inferior[i][j] /= matriz_superior[j][j];
     };
     for(int j=0; j<size; ++j){
         for(int i=0; i<j; ++i){
-            matriz_superior[i][j] =  matriz[i][j] - obtener_suma_superior(i,j);
-            matriz_inferior[j][i] = (matriz[j][i] - obtener_suma_inferior(j,i)) / matriz_superior[i][i];
+            calcular_factor_superior(i,j);
+            calcular_factor_inferior(j,i);
         }
-        matriz_superior[j][j] = matriz[j][j] - obtener_suma_superior(j,j);
+        calcular_factor_superior(j,j);
         if(matriz_inferior[j][j] == 0) throw cant_factor_exception();
     }
 }
