fcc2421dc263e40a1c106bc299b88bf9b9a98a40
Laxharoth <zeydenlaxharoth@gmail.com>
2022-08-20 12:15:53 -0500

    Create matrix_like.hpp

---
+++ metodos_numericos/matrix_like/matrix_like.hpp
@@ -0,0 +1,40 @@
+#ifndef MATRIX_LIKE_HPP
+#define MATRIX_LIKE_HPP
+template <class T>
+class array_like{
+public:
+    virtual T &operator[](const int &row) = 0;
+};
+template <class T>
+class matrix_like{
+public:
+    virtual array_like<T> &operator[](const int &row) = 0;
+};
+
+template <class T>
+class marray: public array_like<T>{
+    public:
+    int &rows,&cols;
+    int row;
+    T *data;
+    marray(T *data, int &rows, int &cols)
+        : data(data), rows(rows), cols(cols){};
+    T &operator[](const int &col){
+        return data[ rows * cols + col];
+    }
+};
+template <class T>
+class matrix : public matrix_like<T>{
+    T *data;
+    int rows,cols;
+    marray<T> *array;
+    public:
+    matrix(const int &rows, const int &cols) : rows(rows), cols(cols){
+        array = new marray<T>(this->data, this->rows, this->cols);
+        data = new T[rows*cols];
+    };
+    ~matrix(){ delete[] data; delete array;}
+    marray<T> &operator[](const int &row) { array->row = row; return *array; }
+};
+
+#endif /* MATRIX_LIKE_HPP */
