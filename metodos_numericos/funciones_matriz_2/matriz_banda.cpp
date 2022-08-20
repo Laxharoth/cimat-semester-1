410b8468e57d275710ae210299400e70d85e6bd8
Laxharoth <zeydenlaxharoth@gmail.com>
2022-08-19 21:43:21 -0500

    Small optimization
    Added row_wrapper to MatrizBanda to prevent the creation of an object  with each call to MatrizBanda::get
    
    fixed row_wrapper::get when it should return 0 and the upper part of the band

--- metodos_numericos/funciones_matriz_2/matriz_banda.cpp
+++ metodos_numericos/funciones_matriz_2/matriz_banda.cpp
@@ -1,37 +1,39 @@
 #include "matriz_banda.hpp"
 
-MatrizBanda::MatrizBanda(const int &left, const int &right, const int &size){
+MatrizBanda::MatrizBanda(const int &left, const int &right, const int &size):
+    left(left),right(right),size(size){
     matriz = new double *[size];
     for(int i=0; i<size; ++i){
         matriz[i] = new double[left + right +1];
     }
+    this->wrapper = new row_wrapper(this->matriz, this->left, this->right, this->size, 0);
 }
 MatrizBanda::~MatrizBanda(){
+    delete this->wrapper;
     for(int i=0; i<size; ++i){
         delete[] matriz[i];
     }
     delete[] matriz;
 }
 
-MatrizBanda::row_wrapper::row_wrapper(double** &matriz, int &left, int &right, int row)
-    :matriz(matriz),right(right),left(left),row(row){};
+MatrizBanda::row_wrapper::row_wrapper(double** &matriz, int &left, int &right, int &size, int row)
+    :matriz(matriz),right(right),left(left),size(size),row(row){};
 
 double MatrizBanda::row_wrapper::default_value = 0;
-
-double &MatrizBanda::get(const int &row, const int &col){
-    return row_wrapper(matriz, left, right, row).get(col);
+MatrizBanda::row_wrapper &MatrizBanda::get(const int &row){
+    this->wrapper->row = row;
+    return *(this->wrapper);
 }
-MatrizBanda::row_wrapper MatrizBanda::operator[](const int &row){
-    return row_wrapper(matriz, left, right, row);
+MatrizBanda::row_wrapper &MatrizBanda::operator[](const int &row){
+    return this->get(row);
 }
-
 double &MatrizBanda::row_wrapper::get(const int &col){
-    if(row > left - col || col > right- row){ 
+    if( (row > col && row - col > left ) || ( col > row && col - row > right ) ){ 
         default_value = 0;
         return default_value;
     }
     if(row == col){ return matriz[row][left]; }
-    if(row < col){return matriz[col + 1][ left + col - row ]; }
+    if(row < col){return matriz[col][left + col - row ]; }
     return matriz[col][ left - row  + col ];
 }
 double &MatrizBanda::row_wrapper::operator[](const int &col){
--- metodos_numericos/funciones_matriz_2/matriz_banda.hpp
+++ metodos_numericos/funciones_matriz_2/matriz_banda.hpp
@@ -2,21 +2,24 @@
 #define MATRIZ_BANDA_HPP
 
 class MatrizBanda{
+    class row_wrapper;
     double** matriz;
+    row_wrapper *wrapper;
     int left{}, right{}, size{};
 class row_wrapper{
     static double default_value;
     double** &matriz;
-    int &left, &right;
+    int &left, &right, &size;
     int row;
 public:
-    row_wrapper(double** &matriz, int &left, int &right, int row);
+    row_wrapper(double** &matriz, int &left, int &right, int &size, int row);
     double &get(const int &col);
     double & operator[](const int &col);
+    friend class MatrizBanda;
 };
 public:
-    double &get(const int &row, const int &col);
-    row_wrapper operator[](const int &row);
+    row_wrapper &get(const int &row);
+    row_wrapper &operator[](const int &row);
     MatrizBanda(const int &left, const int &right, const int &size);
     ~MatrizBanda();
 };
