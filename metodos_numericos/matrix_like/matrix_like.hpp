c101e53c7d914503e5f6a432020488a73a9fff36
Laxharoth <zeydenlaxharoth@gmail.com>
2022-08-20 20:38:41 -0500

    Added initializer_list constructor to matrix

--- metodos_numericos/matrix_like/matrix_like.hpp
+++ metodos_numericos/matrix_like/matrix_like.hpp
@@ -1,5 +1,8 @@
 #ifndef MATRIX_LIKE_HPP
 #define MATRIX_LIKE_HPP
+
+#include <initializer_list>
+
 template <class T>
 class array_like{
 public:
@@ -32,6 +35,25 @@ class matrix : public matrix_like<T>{
         data = new T[rows*cols];
         array = new marray<T>(this->data, this->rows, this->cols);
     };
+    matrix(std::initializer_list<std::initializer_list<T>> l){
+        this->rows = l.size();
+        this->cols = 0;
+        for(auto i = l.begin(); i != l.end(); ++i){
+            if( (*i).size() > this->cols ){
+                this->cols = (*i).size();
+            }
+        }
+        data = new T[rows*cols];
+        this->array = new marray<T>(this->data, this->rows, this->cols);
+        int i{0}, j{0};
+        static int asdfds = 0;
+        for(auto it_i = l.begin(); i < this->rows; ++i, ++it_i){
+            for(auto it_j = (*it_i).begin(); j < this->cols; ++j, ++it_j){
+                (*this)[i][j] = *it_j;
+            }
+            j = 0;
+        }
+    }
     ~matrix(){ delete array; delete[] data; }
     marray<T> &operator[](const int &row) { array->row = row; return *array; }
 };
