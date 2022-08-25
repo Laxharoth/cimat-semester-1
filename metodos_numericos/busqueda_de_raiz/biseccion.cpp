f337bb5f3b33f7b0461d380a77eeedf1d98682c8
Laxharoth <zeydenlaxharoth@gmail.com>
2022-08-24 20:06:05 -0500

    added parameter to recover iterations and last error

--- metodos_numericos/busqueda_de_raiz/biseccion.cpp
+++ metodos_numericos/busqueda_de_raiz/biseccion.cpp
@@ -10,7 +10,10 @@
 
 const char* ERROR_BAD_LIMITS = "5672ba31-9e2a-4796-bf7e-dc61acd048bb";
 
-double biseccion( FunctionWrapper *funcion, double x_inferior, double x_superior ){
+double biseccion( FunctionWrapper *funcion, double x_inferior, double x_superior, int max_iter, int *iteraciones, double *error ){
+    auto calc_err = [](double &anterior, double &actual){ return std::abs((actual - anterior))/std::abs(actual);};
+    int inner_iter{0};
+    int *real_iter;
     if( x_inferior == x_superior ) throw ERROR_BAD_LIMITS;
     if( x_inferior > x_superior ){ std::swap( x_inferior, x_superior); }
     double y_inferior = funcion->eval( x_inferior );
@@ -18,14 +21,24 @@ double biseccion( FunctionWrapper *funcion, double x_inferior, double x_superior
     if( std::abs(y_inferior) <= TOLERANCIA) return x_inferior;
     if( std::abs(y_superior) <= TOLERANCIA) return x_superior;
     double x_medio{}, y_medio{};
-    while(1){
+    real_iter = &inner_iter;
+    if(iteraciones != nullptr){ (*iteraciones) = 0; real_iter = iteraciones; }
+    while((*real_iter) < max_iter){
+        ++(*real_iter);
         if( y_inferior * y_superior > 0 ) throw ERROR_BAD_LIMITS;
         x_medio = ( x_inferior + x_superior ) / 2;
         y_medio = funcion->eval(x_medio);
         if(std::abs(y_medio) <= TOLERANCIA) return x_medio;
-        if(y_medio * y_inferior > 0) x_inferior = x_medio;
-        else x_superior = x_medio;
+        if(y_medio * y_inferior > 0) {
+            if(error != nullptr) (*error) = calc_err(x_inferior, x_medio);
+            x_inferior = x_medio;
+        }
+        else{
+            if(error != nullptr) (*error) = calc_err(x_superior, x_medio);
+            x_superior = x_medio;
+        } 
     }
+    return x_medio;
 }
 
 #endif /* BISECCION_CPP */
--- metodos_numericos/busqueda_de_raiz/newton.cpp
+++ metodos_numericos/busqueda_de_raiz/newton.cpp
@@ -7,14 +7,24 @@
 #define TOLERANCIA 10e-18
 #endif
 
-double newton( FunctionWrapper *funcion, double x_inicial ){
-    Derivative derivada(funcion);
+double newton( FunctionWrapper *funcion, double x_inicial, int max_iter, int *iteraciones, double *error ){
+    auto calc_err = [](const double &anterior,const double &actual){ return std::abs((actual - anterior))/std::abs(actual);};
+    int inner_iter{0};
+    int *real_iter;
     double y_actual{};
-    while(1){
+    Derivative derivada(funcion);
+    real_iter = &inner_iter;
+    if(iteraciones != nullptr){ (*iteraciones) = 0; real_iter = iteraciones; }
+    while((*real_iter) < max_iter){
+        ++(*real_iter);
         y_actual = funcion->eval(x_inicial);
-        if(std::abs(y_actual) <= TOLERANCIA) return y_actual;
+        if(std::abs(y_actual) <= TOLERANCIA) return x_inicial;
+        if(error != nullptr){
+            (*error)=calc_err(x_inicial, x_inicial - funcion->eval(x_inicial) / derivada.eval(x_inicial));
+        }
         x_inicial -= funcion->eval(x_inicial) / derivada.eval(x_inicial);
     }
+    return x_inicial;
 }
 
 #endif /* NEWTON_CPP */
