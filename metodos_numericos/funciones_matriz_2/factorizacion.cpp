57293b0792dbe4836c1c6a10527f2ae273f64d07
Laxharoth <zeydenlaxharoth@gmail.com>
2022-08-25 17:20:51 -0500

    Fixed LDU factorization

--- metodos_numericos/funciones_matriz_2/factorizacion.cpp
+++ metodos_numericos/funciones_matriz_2/factorizacion.cpp
@@ -2,7 +2,25 @@
 #include "factorizacion.hpp"
 
 void metodo_de_crout(matrix_like<double> &matriz, matrix_like<double> &matriz_inferior, matrix_like<double> &matriz_superior, const int &size){
-    factorizacion_LDU(matriz, matriz_inferior, matriz_inferior, matriz_superior, size);
+    auto calcular_factor_inferior = [&](const int &i, const int &j){
+        matriz_inferior[i][j] = matriz[i][j];
+        for(int k = 0; k <= j-1; ++k)
+            matriz_inferior[i][j] -= matriz_inferior[i][k] * matriz_superior[k][j]; 
+    };
+    auto calcular_factor_superior = [&](const int &i, const int &j){
+        matriz_superior[i][j] = matriz[i][j];
+        for(int k = 0; k <= i-1; ++k)
+            matriz_superior[i][j] -= matriz_inferior[i][k] * matriz_superior[k][j];         
+        matriz_superior[i][j] /= matriz_inferior[i][i];
+    };
+    for(int i=0; i<size; ++i){
+        for(int j=0; j<i; ++j){
+            calcular_factor_inferior(i,j);
+            calcular_factor_superior(j,i);
+        }
+        calcular_factor_inferior(i,i);
+        if(matriz_inferior[i][i] == 0) throw cant_factor_exception();
+    }
 }
 void metodo_de_doolittle(matrix_like<double> &matriz, matrix_like<double> &matriz_inferior, matrix_like<double> &matriz_superior, const int &size){
     auto calcular_factor_superior = [&](const int &i, const int &j){
