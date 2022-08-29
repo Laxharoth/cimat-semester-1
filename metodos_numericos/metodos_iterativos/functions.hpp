6c899fae2960967b19e980a1d4291cda37465d8d
Laxharoth <zeydenlaxharoth@gmail.com>
2022-08-29 16:17:57 -0500

    created and tested tested functions

---
+++ metodos_numericos/metodos_iterativos/functions.hpp
@@ -0,0 +1,9 @@
+#include "matrix_like/real_matrix.hpp"
+#include "funcion_matriz.hpp"
+
+void factor_cholesky(mymtx::RealMatrix &matix, mymtx::RealMatrix &triangular);
+void factor_cholesky_tridiag(mymtx::RealMatrix &matix, mymtx::RealMatrix &triangular);
+void solve_jacobi(mymtx::RealMatrix &matix, mymtx::RealVector &variables, mymtx::RealVector &solutions);
+void solve_gauss_seidel(mymtx::RealMatrix &matix, mymtx::RealVector &variables, mymtx::RealVector &solutions);
+void solve_cholesky(mymtx::RealMatrix &cholesky_factored,mymtx::RealVector &variables, mymtx::RealVector &solutions);
+
