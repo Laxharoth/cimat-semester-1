4db5b81cf04bf2705b7d53bca3e5fc6c1d460333
Laxharoth <zeydenlaxharoth@gmail.com>
2022-09-20 16:42:44 -0500

    Update print.cpp

--- metodos_numericos/eigen_vectores/matrix_like/print.cpp
+++ metodos_numericos/eigen_vectores/matrix_like/print.cpp
@@ -1,29 +1,34 @@
 #include "real_matrix.hpp"
 #include <iostream>
 
-void printm(const mymtx::RealMatrix &m){
+void printm(const mymtx::RealMatrix &m, std::ostream& out){
     for (size_t i = 0; i < m.shape_y; ++i)
     {
         for( auto j = m.begin(i); j < m.end(i); ++j){
-            std::cout << *j << " ";
+            out << *j << " ";
         }
-        std::cout << std::endl;
+        out << std::endl;
     }
     
 }
-void printm(const mymtx::MatrixTraspose &m){
+void printm(const mymtx::RealMatrix &m){ printm(m,std::cout);}
+void printm(const mymtx::MatrixTraspose &m, std::ostream& out){
     for (size_t i = 0; i < m.shape_y; ++i)
     {
         for(size_t j = 0; j < m.shape_x; ++j){
-            std::cout << m(i,j) << " ";
+            out << m(i,j) << " ";
         }
-        std::cout << std::endl;
+        out << std::endl;
     }
     
 }
-void printv(const mymtx::RealVector &m){
+void printm(const mymtx::MatrixTraspose &m){ printm(m,std::cout);}
+void printv(const mymtx::RealVector &m, std::ostream& out){
     for( auto j = m.begin(); j < m.end(); ++j){
-            std::cout << *j << " ";
+            out << *j << " ";
     }
-    std::cout << std::endl;
+    out << std::endl;
+}
+void printv(const mymtx::RealVector &m){
+    printv(m,std::cout);
 }
