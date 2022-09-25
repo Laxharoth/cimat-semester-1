751da35eb9cfdeb09517b6e0fc1c427e86641162
Laxharoth <zeydenlaxharoth@gmail.com>
2022-09-24 20:31:16 -0500

    Update jacobi_main.cpp

--- metodos_numericos/eigen_vectores/jacobi_main.cpp
+++ metodos_numericos/eigen_vectores/jacobi_main.cpp
@@ -17,26 +17,31 @@ int main(int argc, char const *argv[]){
     // auto f = std::ofstream("jacobi_out.txt");
     // macros::out = &f;
 
-    auto mtx = mymtx::RealMatrix::tridiag(SIZE,1,-2,1);
+    // auto mtx = mymtx::RealMatrix::tridiag(SIZE,1,-2,1);
+    mymtx::RealMatrix mtx{
+        {1,2,3},
+        {2,4,5},
+        {3,5,6},
+    };
     mtx*=-1;
-    mymtx::RealMatrix eigvec(SIZE,SIZE);
-    mymtx::RealVector eigval(SIZE);
+    mymtx::RealMatrix eigvec(mtx.shape_x,mtx.shape_x);
+    mymtx::RealVector eigval(mtx.shape_x);
     ANNOUNCE_TEST("jacobi eigen")
     measure_time(
         jacobi_eigen(mtx, eigval, eigvec,10000);
     )
-    eigen_index indexs[SIZE];
-    for(int i = 0; i < SIZE; i++){
+    eigen_index indexs[mtx.shape_x];
+    for(int i = 0; i < mtx.shape_x; i++){
         indexs[i].index = i;
         indexs[i].val = eigval[i];
     }
     eigvec = mymtx::RealMatrix::traspose(eigvec);
-    std::sort(indexs,indexs + SIZE, [](const eigen_index&a, const eigen_index&b){ return std::abs(a.val) < std::abs(b.val); } );
+    std::sort(indexs,indexs + mtx.shape_x, [](const eigen_index&a, const eigen_index&b){ return std::abs(a.val) < std::abs(b.val); } );
     strm_out( "min value:" << eigval[indexs[0].index] );
-    strm_out( "max value:" << eigval[indexs[SIZE - 1].index] );
+    strm_out( "max value:" << eigval[indexs[mtx.shape_x - 1].index] );
     mymtx::RealVector::fwrite("min.vec",eigvec[indexs[0].index]);
-    mymtx::RealVector::fwrite("max.vec",eigvec[indexs[SIZE - 1].index]);
-    if(SIZE < 10){
+    mymtx::RealVector::fwrite("max.vec",eigvec[indexs[mtx.shape_x - 1].index]);
+    if(mtx.shape_x < 10){
         printf("values\n");
         printv(eigval);
         printf("vectors\n");
