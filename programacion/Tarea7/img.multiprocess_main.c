ee2613ac2480fd6dcbf5c137a7f7db9df85c7cf1
Laxharoth <zeydenlaxharoth@gmail.com>
2022-09-30 22:09:18 -0500

    rename

--- programacion/Tarea7/img.multiprocess_main.c
+++ programacion/Tarea7/img.multiprocess_main.c
@@ -2,16 +2,17 @@
 #include <sys/wait.h>
 #include <sys/shm.h>
 #include <unistd.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
-#include <string.h>
 #include "median.c"
 #include "pgm1.c"
-
+#include "get_time.h"
 #ifdef _WIN32
 #include <windows.h>
 #else
-#include <unistd.h>
 #endif
 
 #define print_error() printf("ERROR: %s",strerror(errno));
@@ -25,14 +26,33 @@
 #define ERROR_WAIT         -22
 #define ERROR_NOT_IN_ARR   -23
 
+#ifndef WINDOW_SIZE
+#define WINDOW_SIZE 3
+#endif // !WINDOW_SIZE
+
+#ifndef FORK_SIZE
+#define FORK_SIZE 2
+#endif // !FORK_SIZE
+
 // pgm1 se modifico para usar apuntador simple
 void fork_replace(char *mtx,int dim_y, int dim_x, filter fnc, int window_size, size_t forks);
 int main(int argc, char const *argv[]){
     int cols, rows;
     char *img = (char *)pgmRead("fractal_tree.ascii.pgm",&rows, &cols);
     // replace( img, rows,cols,median, 9 );
-    fork_replace( img, rows,cols,median, 9, 2);
+    #ifndef MEASURE_TIME
+    fork_replace( img, rows,cols,median, WINDOW_SIZE, FORK_SIZE);
     pgmWrite("fractal_tree.2.ascii.pgm",rows,cols,(unsigned char*)img,"");
+    #else
+    int window_size[] = {3,5,7,9};
+    int forks[] = {1,2,3,4,9};
+    for (int i = 0; i < 4; i++){
+        for( int j = 0; j < 5 ; j++){
+            long time = (long)GetTimeStamp( fork_replace( img, rows,cols,median, window_size[i], forks[j]); );
+            printf("window:%d, forks:%d, time:%lf\n", window_size[i], forks[j], (double)time*(1e-6));
+        }
+    }
+    #endif // DEBUG
     free(img);
     return 0;
 }
@@ -43,26 +63,25 @@ void fork_replace(char *mtx,int dim_y, int dim_x, filter fnc, int window_size, s
     int start_x, end_x;
     int start_y, end_y;
     key_t key = 5678;
-    pid_t *internal_forks_id;
     char *shrmtx;
     int shmid, pid=0;
     //declare shared memory
     if ((shmid = shmget(key, 
-            forks * sizeof(pid_t) + sizeof(char) *dim_x*dim_y, 
+            sizeof(char) *dim_x*dim_y, 
             IPC_CREAT | 0666)) < 0) {
         RELEASE();
         print_error();
         exit(ERROR_SHMGET);
     }
-    if ((internal_forks_id = shmat(shmid, NULL, 0)) == (void*)-1) {
+    if ((shrmtx = shmat(shmid, NULL, 0)) == (void*)-1) {
         RELEASE();
         print_error();
         exit(ERROR_SHMAT);
     }
-    //copy image in shared memory
-    shrmtx = (char*)(internal_forks_id + forks);
     memcpy(shrmtx,mtx,sizeof(char) *dim_x*dim_y);
-    for (size_t i = 0; i < forks; i++){
+    unsigned int child_n;
+    //create children
+    for (child_n = 0; child_n < forks; child_n++){
         if((pid=fork()) == -1){
             RELEASE();
             print_error();
@@ -70,38 +89,33 @@ void fork_replace(char *mtx,int dim_y, int dim_x, filter fnc, int window_size, s
         }
         //prevent child of forking
         if(pid == 0){ usleep(100);break; }
-        internal_forks_id[i] = pid;
     }
     //child process
     if(pid == 0){
-        int child_num = 0;
-        //get child number
-        while(child_num<forks && 
-              getpid()!=internal_forks_id[child_num])++child_num;
-        if(child_num>=forks) exit(ERROR_NOT_IN_ARR);
+        const int child_id = child_n;
         switch (forks){
             case 2:
                 start_x = 0; end_x = dim_x;
-                start_y = child_num*(dim_y / 2); end_y = (child_num+1)*(dim_y / 2);
+                start_y = child_id*(dim_y / 2); end_y = (child_id+1)*(dim_y / 2);
                 break;
             case 3:
                 start_x = 0; end_x = dim_x;
-                start_y = child_num*(dim_y / 3); end_y = (child_num+1)*(dim_y / 3);
+                start_y = child_id*(dim_y / 3); end_y = (child_id+1)*(dim_y / 3);
                 break;
             case 4:
-                start_x = (child_num/2)*(dim_x / 2);end_x = (child_num/2 +1)*(dim_x / 2);
-                start_y = (child_num/2)*(dim_y / 2);end_y = (child_num/2 +1)*(dim_y / 2);
+                start_x = (child_id%2)*(dim_x / 2);end_x = (child_id%2 +1)*(dim_x / 2);
+                start_y = (child_id/2)*(dim_y / 2);end_y = (child_id/2 +1)*(dim_y / 2);
                 break;
             case 9:
-                start_x = (child_num/3)*(dim_x / 3);end_x = (child_num/3 +1)*(dim_x / 3);
-                start_y = (child_num/3)*(dim_y / 3);end_y = (child_num/3 +1)*(dim_y / 3);
+                start_x = (child_id%3)*(dim_x / 3);end_x = (child_id%3 +1)*(dim_x / 3);
+                start_y = (child_id/3)*(dim_y / 3);end_y = (child_id/3 +1)*(dim_y / 3);
                 break;
             default:
                 printf("case not supported");
                 exit(ERROR_SHMCTL);
                 break;
         }
-        for (size_t i = start_y*child_num; i < end_y; ++i){
+        for (size_t i = start_y; i < end_y; ++i){
             for (size_t j = start_x; j < end_x; ++j){
                 shrmtx[i*dim_x+j] = fnc(shrmtx,i,j,window_size,dim_y,dim_x);
             }
