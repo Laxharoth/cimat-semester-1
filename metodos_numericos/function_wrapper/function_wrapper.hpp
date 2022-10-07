4d2900f99571306d3cfcaed371c74ac4911ecb81
Laxharoth <zeydenlaxharoth@gmail.com>
2022-10-07 09:11:44 -0500

    Update function_wrapper.hpp

--- metodos_numericos/function_wrapper/function_wrapper.hpp
+++ metodos_numericos/function_wrapper/function_wrapper.hpp
@@ -5,29 +5,35 @@
 #define DELTA_X 0.00001
 #endif
 
-class FunctionWrapper{
-    public:
-    virtual double eval(const double &x) = 0;
-    virtual double eval(const double &x) const = 0;
+class FunctionWrapper {
+public:
+  virtual double eval(const double &x) = 0;
+  virtual double eval(const double &x) const = 0;
 };
 
-class Derivative : public FunctionWrapper{
-    FunctionWrapper *original_function;
-    public:
-    Derivative(FunctionWrapper *original);
-    double eval(const double &x);
-    double eval(const double &x) const;
+class Derivative : public FunctionWrapper {
+  FunctionWrapper *original_function;
+
+public:
+  Derivative(FunctionWrapper *original);
+  double eval(const double &x);
+  double eval(const double &x) const;
 };
 
-class Count : public FunctionWrapper{
-    double current,step;
-    public:
-    Count();
-    Count(double start);
-    Count(double start,double step);
-    Count(double start, double end,unsigned int steps);
-    double eval(const double &x);
-    double eval(const double &x) const;
+class Count : public FunctionWrapper {
+  double current, step;
+
+public:
+  Count();
+  Count(double start);
+  Count(double start, double step);
+  Count(double start, double end, unsigned int steps);
+  double eval(const double &x);
+  double eval(const double &x) const;
 };
 
+struct point {
+  double x;
+  double y;
+};
 #endif /* FUNCTION_WRAPPER_HPP */
